1fa8de5dfe4e2a1e31476320b60b156ffc79a115
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-06 20:05:30 -0700

    Add files via upload

---
+++ Class-8/app.js
@@ -0,0 +1,17 @@
+//homework
+//1.post-increment = i++ or pre-increment = ++i mei difference/unary operator
+//2.print table of 4, 4*1=4
+//for (let i = 10; i > 0; i++){   //infinite loop
+//  console.log("sorry teacher", i);
+//}
+//Array
+var student_name = ["Ali", "Umer", "Adil"]; //each item in an array is called an element
+console.log(student_name); //[0],[1],[2] index number
+//student_name.push("Aneeq") //aik or variable push ya add krwane ke liye ye function use hoga
+//console.log(student_name); //phir dobara console krwaenge
+//student_name.pop() //array ka sbse last element pop hojaega 
+//console.log(student_name);
+//student_name.unshift("Aneeq") //start mei aik new element add krdeta hai
+//console.log(student_name);
+student_name.shift(); //start se name remove krta hai
+console.log(student_name);
